2297dbc5d1fa2abd211695d42ea4710daf3cb0cd
Adarshclk <adarshaadarsh180@gmail.com>
2025-03-21 15:30:26 +0530

    pushing all the code to git

---
+++ ProdRunner.java
@@ -0,0 +1,100 @@
+class ProdRunner{
+public static void main(String... args){
+String name;
+name="pumaShoes";
+ double cost=Prod.getPriceByProduct(name);
+System.out.println("name of the product :"+name+" price is"+ cost);
+
+name="pumaJacket";
+ cost=Prod.getPriceByProduct(name);
+System.out.println("name of the product :"+name+" price is"+ cost);
+
+name="fastrackWatch";
+ cost=Prod.getPriceByProduct(name);
+System.out.println("name of the product :"+name+" price is"+ cost);
+
+name="chair";
+ cost=Prod.getPriceByProduct(name);
+System.out.println("name of the product :"+name+" price is "+ cost);
+
+name=" fastrackWatch1";
+ cost=Prod.getPriceByProduct(name);
+System.out.println("name of the product :"+name+" price is "+ cost);
+
+name="spects";
+ cost=Prod.getPriceByProduct(name);
+System.out.println("name of the product :"+name+" price is "+ cost);
+
+name="comb";
+cost=Prod.getPriceByProduct(name);
+System.out.println("name of the product :"+name+" price is "+ cost);
+
+
+name="hairDrier";
+ cost=Prod.getPriceByProduct(name);
+System.out.println("name of the product :"+name+" price is "+ cost);
+
+name="Fogg";
+ cost=Prod.getPriceByProduct(name);
+System.out.println("name of the product :"+name+" price is "+ cost);
+
+name="wallet";
+cost=Prod.getPriceByProduct(name);
+System.out.println("name of the product :"+name+" price is "+ cost);
+
+name="shirt";
+ cost=Prod.getPriceByProduct(name);
+System.out.println("name of the product :"+name+" price is "+cost);
+
+name="pumawatch";
+cost=Prod.getPriceByProduct(name);
+System.out.println("name of the product :"+name+" price is "+cost);
+
+name="mixer";
+cost=Prod.getPriceByProduct(name);
+System.out.println("name of the product :"+name+" price is "+cost);
+
+name="woodlandWallet";
+ cost=Prod.getPriceByProduct(name);
+System.out.println("name of the product :"+name+" price is "+cost);
+
+name="jar";
+ cost=Prod.getPriceByProduct(name);
+System.out.println("name of the product :"+name+" price is "+cost);
+
+name="oven";
+ cost=Prod.getPriceByProduct(name);
+System.out.println("name of the product :"+name+" price is "+cost);
+
+name="AirCooler";
+ cost=Prod.getPriceByProduct(name);
+System.out.println("name of the product :"+name+" price is "+cost);
+
+name="AppleLaptop";
+ cost=Prod.getPriceByProduct(name);
+System.out.println("name of the product :"+name+" price is "+cost);
+
+name="HPLaptop";
+ cost=Prod.getPriceByProduct(name);
+System.out.println("name of the product :"+name+" price is "+cost);
+
+name="LenevoLaptop";
+ cost=Prod.getPriceByProduct(name);
+System.out.println("name of the product :"+name+" price is "+cost);
+
+name="Charger";
+ cost=Prod.getPriceByProduct(name);
+System.out.println("name of the product :"+name+" price is "+cost);
+
+name="Fridge";
+ cost=Prod.getPriceByProduct(name); 
+System.out.println("name of the product :"+name+" price is "+cost);
+
+name="washingMachine";
+ cost=Prod.getPriceByProduct(name);
+System.out.println("name of the product :"+name+" price is "+cost);
+}
+
+}
+
+
